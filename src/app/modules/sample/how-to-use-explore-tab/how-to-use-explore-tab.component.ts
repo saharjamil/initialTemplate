1a8fa6cbd4fc4690c415aa009e6454552fbbc428
saharjamil <s.vesaaghatijamil@gmail.com>
2025-07-01 13:40:57 +0330

    update:: icon

--- src/app/modules/sample/how-to-use-explore-tab/how-to-use-explore-tab.component.ts
+++ src/app/modules/sample/how-to-use-explore-tab/how-to-use-explore-tab.component.ts
@@ -4,13 +4,21 @@ import { TabInterface } from '../../../core/interfaces/tabInterface';
 import { SampleListComponent } from './sample-list/sample-list.component';
 @Component({
   selector: 'app-how-to-use-explore-tab',
-  standalone:false,
+  standalone: false,
   templateUrl: './how-to-use-explore-tab.component.html',
-  styleUrl: './how-to-use-explore-tab.component.scss'
+  styleUrl: './how-to-use-explore-tab.component.scss',
 })
 export class HowToUseExploreTabComponent {
   setting: AppSetting = new AppSetting();
   defaultTab: TabInterface[] = [
-      { id: 'ticketList', title: 'لیست', icon: 'bi-envelope', active: true, componentRef: SampleListComponent, data: {}, closable: false },
+    {
+      id: 'ticketList',
+      title: 'لیست',
+      icon: 'fileText',
+      active: true,
+      componentRef: SampleListComponent,
+      data: {},
+      closable: false,
+    },
   ];
 }
